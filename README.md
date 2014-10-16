2dbfdab2dff4b37ed07fd84b3c78d9ab86c756ad
Jimi Smoot <jsfour@gmail.com>
2014-10-16 15:45:10 -0700

    fist commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a school project for a marketing class I took in college.
---
+++ sharp_aleane/photos.js
@@ -0,0 +1,104 @@
+WPGgallery = new Object();
+WPGgallery.name = "Adobe%20Web%20Photo%20Gallery";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "4/19/2007"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "DSC09465.jpg";
+gPhotos[0].ImageWidth = 350;
+gPhotos[0].ImageHeight = 233;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 50;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "DSC09488.jpg";
+gPhotos[1].ImageWidth = 234;
+gPhotos[1].ImageHeight = 350;
+gPhotos[1].ThumbWidth = 50;
+gPhotos[1].ThumbHeight = 75;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "DSC09561.jpg";
+gPhotos[2].ImageWidth = 350;
+gPhotos[2].ImageHeight = 233;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 50;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "art_bk.jpg";
+gPhotos[3].ImageWidth = 350;
+gPhotos[3].ImageHeight = 233;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 50;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "arta.jpg";
+gPhotos[4].ImageWidth = 350;
+gPhotos[4].ImageHeight = 300;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 64;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "bd2.jpg";
+gPhotos[5].ImageWidth = 350;
+gPhotos[5].ImageHeight = 233;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 50;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "bd2_.jpg";
+gPhotos[6].ImageWidth = 350;
+gPhotos[6].ImageHeight = 104;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 22;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "breakdance.jpg";
+gPhotos[7].ImageWidth = 350;
+gPhotos[7].ImageHeight = 234;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 50;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "mann.jpg";
+gPhotos[8].ImageWidth = 233;
+gPhotos[8].ImageHeight = 350;
+gPhotos[8].ThumbWidth = 50;
+gPhotos[8].ThumbHeight = 75;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "poets.jpg";
+gPhotos[9].ImageWidth = 350;
+gPhotos[9].ImageHeight = 233;
+gPhotos[9].ThumbWidth = 75;
+gPhotos[9].ThumbHeight = 50;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "table.jpg";
+gPhotos[10].ImageWidth = 350;
+gPhotos[10].ImageHeight = 233;
+gPhotos[10].ThumbWidth = 75;
+gPhotos[10].ThumbHeight = 50;
+gPhotos[10].meta = new Object();
+
